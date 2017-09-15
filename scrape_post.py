db9f46847fe0abc388bad185c48b31c70b153014
Ryan <ryan.j.porterr@gmail.com>
2017-09-15 06:59:42 -0300

    Work on gathering information on job posts
    Currently collects domains of actual posts

---
+++ scrape_post.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse
+from collections import defaultdict
+from time import sleep
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+POST_URLS = 'post_urls.txt'
+
+def merge_link(url: str) -> str:
+    """merge url with https://www.indeed.ca/"""
+    return 'https://www.indeed.ca' + url
+
+class FollowUp(scrapy.Spider):
+    """spider to follow links in post_urls"""
+    name = 'followup'
+    domains = defaultdict(int)
+
+    start_urls = list(map(merge_link, open(POST_URLS, 'r')))
+
+    def parse(self, response):
+        sleep(2)
+        dom = urlparse(response.url).netloc
+        self.domains[dom] += 1
+
+
+if __name__ == "__main__":
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    })
+    dom_count = FollowUp()
+    process.crawl(dom_count)
+    process.start()
+    log = open('domains.txt', 'w')
+    for key, value in dom_count.domains.items():
+        print(key + ', ' + str(value), file = log) 
