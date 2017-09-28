56539f47c7ba1dddff789d904095c3a8cca293cb
Ryan <ryan.j.porterr@gmail.com>
2017-09-28 17:07:16 -0300

    Reduced hardcoding in target_search.py

--- spiders/target_search.py
+++ spiders/target_search.py
@@ -3,46 +3,58 @@ import re
 from time import sleep
 import bs4 as bs
 import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess
 
-TARGET = open('job_query.txt').readline()
-OUTPUT = open('post_urls.txt', 'w')
 
-class Spider(scrapy.Spider):
-    """subclass of scrapy.Spider for scraping indeed results"""
-    name = 'job-posts'
-    start_urls = [
-        TARGET,
-    ]
-    custom_settings = {'LOG_LEVEL': 'INFO'}    
-    def parse(self, response):
-        """main method of the spider class collects input and new content"""
-        # collect job posts
-        print('Scraping ' + response.url + '...')
-        soup = bs.BeautifulSoup(response.body, 'lxml') 
-
-        for post in soup.find_all(attrs={'class': 'row'}): # loops through each tag with row class
-            #get posting url
-            url = post.a['href']
-
-            print(url, file=OUTPUT) # prints the url into the ouptut file
-
-        sleep(2) # wait 2 sec to space out requests
-
-        index = int(re.search(r'start=(\d+)', response.url).group(1)) # gets the offset of page
-
-        # find all np tags (prev, next buttons)
-        next_page = len(soup.find_all(attrs={'np'}))
-        next_q = index + 10
+def run_target_search(target:str, output:str):
+    """ 'target' is the url of the job search. 
+        'output' is the name of the output file. 
         
-        if next_page > 1 or index < 10: # after the first page stop if there is no next page
-            next_page = response.url.replace(f'start={index}', f'start={next_q}') # construct URL
-            return response.follow(next_page, self.parse)
+        Executes the spider dumping the results to output        
+    """
+    outfile = open(output, 'w')
 
+    class Target_Search(scrapy.Spider):
+        """subclass of scrapy.Spider for scraping indeed results"""
+        name = 'job-posts'
+        start_urls = [
+            target,
+        ]
+        custom_settings = {'LOG_LEVEL': 'INFO'}    
+        def parse(self, response):
+            """main method of the spider class collects input and new content"""
+            # collect job posts
+            print('Scraping ' + response.url + '...')
+            soup = bs.BeautifulSoup(response.body, 'lxml') 
 
-if __name__ == "__main__":
-    from scrapy.crawler import CrawlerProcess
+            for post in soup.find_all(attrs={'class': 'row'}): # loops through each tag with row class
+                #get posting url
+                url = post.a['href']
+
+                print(url, file=outfile) # prints the url into the ouptut file
+
+            sleep(2) # wait 2 sec to space out requests
+
+            index = int(re.search(r'start=(\d+)', response.url).group(1)) # gets the offset of page
+
+            # find all np tags (prev, next buttons)
+            next_page = len(soup.find_all(attrs={'np'}))
+            next_q = index + 10
+            
+            if next_page > 1 or index < 10: # after the first page stop if there is no next page
+                next_page = response.url.replace(f'start={index}', f'start={next_q}') # construct URL
+                return response.follow(next_page, self.parse)
+
+    
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
     })
-    process.crawl(Spider)
+    process.crawl(Target_Search)
     process.start()
+    
+
+if __name__ == "__main__":
+    TARGET = open('../job_query.txt').readline()
+    OUTPUT = 'post_urls.txt'
+    run_target_search(TARGET, OUTPUT)
