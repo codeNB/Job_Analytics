c39a248ebd16c64278d73d7628a9ffa634df1360
Ryan <ryan.j.porterr@gmail.com>
2017-09-29 09:44:04 -0300

    tested dedup.py

--- util/dedup.py
+++ util/dedup.py
@@ -13,10 +13,12 @@ def dedup_post_urls(file: str, outfile: str = 'cleaned_post_urls.txt'):
     # add url to outfile if not encountered before
     for line in urls_in:
         if line not in seen:
-            print(line, file=urls_out)
+            print(line, file=urls_out, end='')
             seen.add(line)
         else:
             count += 1
 
     print("found {0} duplicates".format(count))
     
+if __name__ == "__main__":
+    dedup_post_urls(file='../spiders/post_urls.txt', outfile='test.txt')
