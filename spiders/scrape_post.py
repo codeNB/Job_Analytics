f8531abde5938029485f23c7bdebbfe326074df6
Ryan <ryan.j.porterr@gmail.com>
2017-10-09 09:21:23 -0300

    Spiders are working indipendantly.

--- spiders/scrape_post.py
+++ spiders/scrape_post.py
@@ -1,27 +1,48 @@
+"""Second spider to run, gathers text from job post entries"""
 from urllib.parse import urlparse
-from collections import defaultdict
 from time import sleep
+from bs4 import BeautifulSoup
+from bs4.element import Comment
 import scrapy
-from scrapy.crawler import CrawlerProcess
 
-def merge_link(url:str) -> str:
+def merge_link(url: str) -> str:
     """merge url with https://www.indeed.ca/"""
     return 'https://www.indeed.ca' + url
 
-def create_followup(post_urls, outfile):
-    """creates followup spider obj"""
+def tag_visible(element):
+    """tests if element is a visible element on webpage"""
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    elif isinstance(element, Comment):
+        return False
+    else:
+        return True
+
+def text_from_html(body: str):
+    """selects visible text from html"""
+    soup = BeautifulSoup(body, 'html.parser')
+    text = soup.findAll(text=True)
+    visible_text = filter(tag_visible, text)
+    return u' '.join(t.strip() for t in visible_text) 
+
+def create_followup(post_urls: str, outfile: str):
+    """creates followup spider obj
+        post_urls: file name of a file containing links to jobposts
+        outfile: desired output file
+
+    """
+    out = open(outfile, mode='w')
 
     class FollowUp(scrapy.Spider):
         """spider to follow links in post_urls"""
         name = 'followup'
-        domains = defaultdict(int) # default of domains['some key'] = 0
-
+        
         start_urls = list(map(merge_link, open(post_urls, 'r'))) #creates list from POST_URLS
 
         def parse(self, response):
             """defines spider behavior TODO change so that it extracts data from post pages"""
             sleep(2)
-            dom = urlparse(response.url).netloc #gets the domain of the response
-            self.domains[dom] += 1 # counts occurrences of that domain 
+            print(text_from_html(response.body), file=out)
+            
 
     return FollowUp
--- spiders/target_search.py
+++ spiders/target_search.py
@@ -5,6 +5,7 @@ from time import sleep
 import bs4 as bs
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from twisted.internet import process
 
 def create_target_search(target:str, output:str):
     """ 'target' is the url of the job search. 
@@ -33,7 +34,7 @@ def create_target_search(target:str, output:str):
                 url = post.a['href']
 
                 print(url, file=outfile) # prints the url into the ouptut file
-
+                
             sleep(2) # wait 2 sec to space out requests
 
             index = int(re.search(r'start=(\d+)', response.url).group(1)) # gets the offset of page
@@ -41,9 +42,8 @@ def create_target_search(target:str, output:str):
             # find all np tags (prev, next buttons)
             next_page = len(soup.find_all(attrs={'np'}))
             next_q = index + 10
-            
+
             if next_page > 1 or index < 10: # after the first page stop if there is no next page
                 next_page = response.url.replace(f'start={index}', f'start={next_q}') # construct URL
-                return response.follow(next_page, self.parse)
-
+                yield response.follow(next_page, self.parse)
     return Target_Search
